87011481294da9a45089eb65e3d03e28582a3a0f
matheusma37 <mtsma37@gmail.com>
2019-03-04 11:00:31 -0300

    Iniciando o projeto.

---
+++ src/main/java/com/iff/loo/animewiki/AnimewikiApplication.java
@@ -0,0 +1,13 @@
+package com.iff.loo.animewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimewikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimewikiApplication.class, args);
+	}
+
+}
