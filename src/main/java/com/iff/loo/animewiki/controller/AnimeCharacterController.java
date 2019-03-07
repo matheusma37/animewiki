78bdaeeb6a8eeace85e9ca943461de004a3d4553
matheusma37 <mtsma37@gmail.com>
2019-03-07 18:10:59 -0300

    Adicionando funcionalidade de upload de foto.

--- src/main/java/com/iff/loo/animewiki/controller/AnimeCharacterController.java
+++ src/main/java/com/iff/loo/animewiki/controller/AnimeCharacterController.java
@@ -3,11 +3,19 @@ package com.iff.loo.animewiki.controller;
 import com.iff.loo.animewiki.model.AnimeCharacter;
 import com.iff.loo.animewiki.repository.Animes;
 import com.iff.loo.animewiki.repository.Characters;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -38,7 +46,25 @@ public class AnimeCharacterController {
     }
 
     @RequestMapping(value="",method=RequestMethod.POST)
-    public String save(AnimeCharacter c) {
+    public String save(AnimeCharacter c,
+            @RequestParam("file") MultipartFile imageFile) {
+        if(imageFile.isEmpty()){
+            return "redirect:/characters";
+        }
+        Path absolutePath = Paths.get(".").toAbsolutePath();
+        c.setPhoto(c.getName() + imageFile.getOriginalFilename());
+        try {
+            Files.write(
+                    Paths.get(absolutePath +
+                            "/src/main/resources/static/images/character/" +
+                            c.getName() + imageFile.getOriginalFilename()
+                    ),
+                    imageFile.getBytes()
+            );
+        } catch (IOException ex) {
+            Logger.getLogger(AnimeCharacterController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
         characters.save(c);
         return "redirect:/characters";
     }
--- src/main/java/com/iff/loo/animewiki/controller/AnimeController.java
+++ src/main/java/com/iff/loo/animewiki/controller/AnimeController.java
@@ -2,11 +2,19 @@ package com.iff.loo.animewiki.controller;
 
 import com.iff.loo.animewiki.model.Anime;
 import com.iff.loo.animewiki.repository.Animes;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -32,9 +40,27 @@ public class AnimeController {
     }
 
     @RequestMapping(value="",method=RequestMethod.POST)
-    public String save(Anime a) {
-            animes.save(a);
+    public String save(Anime a,
+            @RequestParam("file") MultipartFile imageFile) {
+        if(imageFile.isEmpty()){
             return "redirect:/animes";
+        }
+        Path absolutePath = Paths.get(".").toAbsolutePath();
+        a.setPhoto(a.getName() + imageFile.getOriginalFilename());
+        try {
+            Files.write(
+                    Paths.get(absolutePath +
+                            "/src/main/resources/static/images/anime/" +
+                            a.getName() + imageFile.getOriginalFilename()
+                    ),
+                    imageFile.getBytes()
+            );
+        } catch (IOException ex) {
+            Logger.getLogger(AnimeCharacterController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        animes.save(a);
+        return "redirect:/animes";
     }
 
     @RequestMapping(value="update/{id}")
